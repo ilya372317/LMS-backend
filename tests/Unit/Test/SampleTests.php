0e12720288877e0cb03effe0e105a79808fe2998
ilya-otinov <ilya.otinov@gmail.com>
2022-07-21 10:55:21 +0700

    configute xdebug

---
+++ tests/Unit/Test/SampleTests.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Unit\Test;
+
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTests extends TestCase
+{
+    public function testEquals(): void
+    {
+        $this->assertEquals(1,1);
+    }
+}
